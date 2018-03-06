462dc634e33c250fc6c7384604b774b5e4d9aba0
hilyatsaniya <hilyatsaniya@yahoo.co.id>
2018-03-06 21:44:00 +0700

    Bagian view Tips

---
+++ Tips.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+     <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/Tips.css">     
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap-theme.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap-theme.min.css">
+
+<!-- jQuery library -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<!-- Latest compiled JavaScript -->
+<script src="<?php echo base_url();?>css/bootstrap.min.js"></script> 
+
+    <title></title>
+  </head>
+  <body>
+  	<section class="Grid">
+  <div class="tips-1">
+	<center><h>Tips for</h></center>
+		<p>Ini adalah tips blablablab</p>
+
+  </div>
+
+  <div class="tips-2">
+	<center><h>Tips for</h></center>
+		<p>Ini adalah tips blablablab</p>
+  </div>
+
+  <div class="tips-3">
+	<center><h>Tips for</h></center>
+		<p>Ini adalah tips blablablab</p>
+  </div>
+
+    <div class="tips-4">
+	<center><h>Tips for</h></center>
+		<p>Ini adalah tips blablablab</p>
+  </div>
+
+    <div class="tips-5">
+	<center><h>Tips for</h></center>
+		<p>Ini adalah tips blablablab</p>
+  </div>
+
+
+    <div class="tips-6">
+	<center><h>Tips for</h></center>
+		<p>Ini adalah tips blablablab</p>
+  </div>
+
+    <div class="tips-7">
+	<center><h>Tips for</h></center>
+		<p>Ini adalah tips blablablab</p>
+  </div>
+
+    <div class="tips-8">
+	<center><h>Tips for</h></center>
+		<p>Ini adalah tips blablablab</p>
+  </div>
+
+</section>
+
+<div class="pagin">
+<a href="#" class="previous"> Previous</a>
+<a href="#" class="next">Next</a>
+</div>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
