f8d0fe1ae5d260e7aa6c67f654ca1690a49b4f04
Arie Brian <ariebrian22@gmail.com>
2018-03-10 22:42:19 +0700

    add database+get tips

---
+++ application/controllers/Tips_Controller.php
@@ -0,0 +1,44 @@
+<?php 
+
+/**
+* 
+*/
+class Tips_Controller extends CI_Controller
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+		$this->load->library('session');
+		$this->load->helper('url');
+		$this->load->helper('form');
+		$this->load->helper('date');
+		$this->load->model('Tips_Model');
+	}
+
+	public function show_tips()
+	{
+		$data['tips'] = $this->Tips_Model->get_all_tips();
+		echo "<pre>";
+		var_dump($data);
+		die();
+		echo "</pre>";
+	}
+
+	public function get_tip($id)
+	{
+		if ($id === NULL) {
+			return false;
+		} else {
+			$tip = array('details' => $this->Tips_Model->get_one_tips($id) , );
+		}
+		echo "<pre>";
+		var_dump($tip);
+		die();
+		echo "</pre>";
+		
+	}
+}
+
+ ?>
