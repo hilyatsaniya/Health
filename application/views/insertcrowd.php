d07081d778fec6781fd6266b016f694288014082
IntanPratiwi <intaaannpraa12@gmail.com>
2018-03-16 23:05:16 +0700

    ya done

--- application/views/insertcrowd.php
+++ application/views/insertcrowd.php
@@ -17,50 +17,50 @@
     <div class="form-group">
       <label class="control-label col-sm-3" for="email">Judul Bantuan</label>
       <div class="col-sm-8">
-        <input type="text" class="form-control"  >
+        <input type="text" class="form-control" name="Judul Bantuan"  >
       </div>
     </div>
     <div class="form-group">
       <label class="control-label col-sm-3" for="pwd">Target Donasi</label>
       <div class="col-sm-8">          
-        <input type="text" class="form-control" >
+        <input type="text" class="form-control" name="Target Donasi">
       </div>
     </div>
     <div class="form-group">
       <label class="control-label col-sm-3" for="pwd">Link Campaign (contoh: healthyhelper.com/helparie)</label>
       <div class="col-sm-8">          
-        <input type="text" class="form-control" >
+        <input type="text" class="form-control" name="Link Campaign">
       </div>
     </div>
     <div class="form-group">
       <label class="control-label col-sm-3" for="pwd">Deadline Campaign</label>
       <div class="col-sm-8">          
-        <input type="date" class="form-control" >
+        <input type="date" class="form-control" name="Deadline Campaign" >
       </div>
     </div>
     <div class="form-group">
       <label class="control-label col-sm-3" for="pwd">Lokasi Penerimaan Dana</label>
       <div class="col-sm-8">          
-        <input type="text" class="form-control" >
+        <input type="text" class="form-control" name="Lokasi Penerimaan Bantuan">
       </div>
     </div>
     <div class="form-group">
       <label class="control-label col-sm-3" for="pwd">Cover Image</label>
       <div class="col-sm-8">          
-        <input type="file" name="images" accept= ".jpg" class="form-control" id="images" >
+        <input type="file" name="images" accept= ".jpg" class="form-control" id="images" name="Cover Image">
       </div>
     </div>
     </div>
     <div class="form-group">
       <label class="control-label col-sm-3" for="pwd">Deskripsi Singkat</label>
       <div class="col-sm-8"> 
-        <textarea class="form-control" rows="4" ></textarea>         
+        <textarea class="form-control" rows="4" name="Deskripsi Singkat" ></textarea>         
       </div>
     </div>
     <div class="form-group">
       <label class="control-label col-sm-3" for="pwd">Deskripsi Lengkap</label>
       <div class="col-sm-8"> 
-        <textarea class="form-control" rows="10" ></textarea>         
+        <textarea class="form-control" rows="10" name="Deskripsi Lengkap"></textarea>         
       </div>
     </div>
     <div class="form-group">        
